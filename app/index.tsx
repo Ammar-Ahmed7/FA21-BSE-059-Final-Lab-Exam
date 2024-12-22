f74414a3087b9148fe259f4f2b54b62c8dfdd5e1
unknown <ammar.ahmed9020@gmail.com>
2024-12-22 22:05:05 +0500

    Updated

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View,Text,SafeAreaView } from "react-native";
+
+export default function Home(){
+  return(
+    <View className="flex-1 justify-center items-center">
+    <Text className="bg-purple-700 text-blue-700 border-2 border-black">hello</Text>
+    </View>
+    
+  );
+}
